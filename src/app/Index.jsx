4592ca4301d439f734bdd28cd2a6b478fb2044f1
Tarcisio Rodrigues <tarcisiorp16@gmail.com>
2021-08-05 18:58:00 -0300

    dependecies

--- src/app/Index.jsx
+++ src/app/Index.jsx
@@ -19,6 +19,7 @@ function App() {
                     <Route exact path="/" component={Home} />
                     <Route path="/users" component={Users} />
                     <Route path="/clients" component={Clients} />
+                    <Route path="/autentication" component={Autentication} />
                     <Redirect from="*" to="/" />
                 </Switch>
             </div>
